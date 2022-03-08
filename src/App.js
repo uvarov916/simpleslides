353a676696f9a220a0e3f7855be0e3351041b5ad
Ivan Uvarov <tonk2012@gmail.com>
2022-03-08 22:44:54 +0300

    2d navigation + fixed overflow

--- src/App.js
+++ src/App.js
@@ -6,36 +6,54 @@ import { useEffect, useState } from "react";
 
 const KEYCODES = {
   LEFT_ARROW: 37,
-  RIGHT_ARROW: 39
+  UP_ARROW: 38,
+  RIGHT_ARROW: 39,
+  DOWN_ARROW: 40
 };
 
 const SLIDES = [
-  {
-    heading: "Slide 1",
-    description: "Description 1"
+  [{
+    heading: "Slide 1.1",
+    description: "Description 1.1"
   },
   {
+    heading: "Slide 1.2",
+    description: "Description 1.2"
+  }],
+  [{
     heading: "Slide 2",
     description: "Description 2"
+  }],
+  [{
+    heading: "Slide 3.1",
+    description: "Description 3.1"
   },
   {
-    heading: "Slide 3",
-    description: "Description 3"
-  }
+    heading: "Slide 3.2",
+    description: "Description 3.2"
+  },
+  {
+    heading: "Slide 3.3",
+    description: "Description 3.3"
+  }],
+  
 ]
 
-function Slide(props) {
+function Slide({slide}) {
   return (
     <div className="slide">
-      <h1 className="slide_heading">{props.heading}</h1>
-      <p className="slide_description">{props.description}</p>
+      <h1 className="slide_heading">{slide.heading}</h1>
+      <p className="slide_description">{slide.description}</p>
     </div>
   )
 }
 
 function App() {
 
-  const [slideIndex, setSlideIndex] = useState(0);
+  const [slideIndex, setSlideIndex] = useState({
+    x: 0,
+    y: 0
+  });
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyboard);
@@ -48,23 +66,37 @@ function App() {
   const handleKeyboard = (e) => {
     switch (e.keyCode) {
       case KEYCODES.LEFT_ARROW:
-        console.log('<-');
-        setSlideIndex(slideIndex - 1);
+        console.log('←');
+        if (slideIndex.x > 0) {
+          setSlideIndex({x: slideIndex.x - 1, y: 0});
+        }
         break;
       case KEYCODES.RIGHT_ARROW:
-        console.log('->');
-        setSlideIndex(slideIndex + 1);
+        console.log('→');
+        if (slideIndex.x < SLIDES.length - 1) {
+          setSlideIndex({x: slideIndex.x + 1, y: 0});
+        }
+        break;
+      case KEYCODES.UP_ARROW:
+        console.log('↑');
+        if (slideIndex.y > 0) {
+          setSlideIndex({x: slideIndex.x, y: slideIndex.y - 1});
+        }
+        break;
+      case KEYCODES.DOWN_ARROW:
+        console.log('↓');
+        if (slideIndex.y < SLIDES[slideIndex.x].length - 1) {
+          setSlideIndex({x: slideIndex.x, y: slideIndex.y + 1});
+        }
         break;
       default: 
-        console.log('bs');
+        console.log(e.keyCode);
     }
   }
 
   return (
     <div className="App">  
-      <Slide 
-        heading={SLIDES[slideIndex].heading} 
-        description={SLIDES[slideIndex].description} />
+      <Slide slide={SLIDES[slideIndex.x][slideIndex.y]} />
     </div>
   );
 }
